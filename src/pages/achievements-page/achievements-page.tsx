4d2c71140e54c605d177634de2094db67768f8b2
Kostya <83646158+Skakodub-K@users.noreply.github.com>
2025-08-12 20:54:40 +0300

    Update achievements-page.tsx

--- src/pages/achievements-page/achievements-page.tsx
+++ src/pages/achievements-page/achievements-page.tsx
@@ -7,19 +7,18 @@ const AchievementsPage = () => {
       title: "Клиентский сервис",
       description: "Лучшие ачивки за клиентский сервис!!!",
       items: [
-        { id: 1, url: "ach1.png", title: "Cool" },
-        { id: 2, url: "ach2.png", title: "New" },
-        { id: 3, url: "ach3.png", title: "Great" },
+        { id: 1, url: "ach1.png", title: "Легенда роста" },
+        { id: 2, url: "ach2.png", title: "Вебинарный дебют" },
+        { id: 3, url: "ach3.png", title: "Первый кейс" },
       ],
     },
     {
       title: "Командное взаимодействие",
       description: "Лучшие ачивки за командное взаимодействие!!!",
       items: [
-        { id: 1, url: "ach4.png", title: "Cool" },
-        { id: 2, url: "ach5.png", title: "New" },
-        { id: 3, url: "ach6.png", title: "Great" },
-        { id: 4, url: "ach7.png", title: "Not bad" },
+        { id: 1, url: "ach4.png", title: "Первые шаги" },
+        { id: 2, url: "ach5.png", title: "Эксперт кейсов" },
+        { id: 3, url: "ach6.png", title: "Мастер обучения" },
       ],
     },
   ];
